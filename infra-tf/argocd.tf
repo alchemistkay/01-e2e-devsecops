8c516bdb49a1bae3ec712b1116f0647f184789ca
AlchemistKay <alchemistkay@k4scloud.com>
2025-06-19 12:43:10 +0000

    code fixes

--- infra-tf/argocd.tf
+++ infra-tf/argocd.tf
@@ -1,15 +1,15 @@
-resource "helm_release" "argocd" {
-  name       = "argocd"
-  namespace  = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  version    = "8.1.1"
-  values = [
-    <<EOF
-        server:
-        service:
-            type: LoadBalancer
-    EOF
-  ]
-  depends_on = [module.eks]
-}
+# resource "helm_release" "argocd" {
+#   name       = "argocd"
+#   namespace  = "argocd"
+#   repository = "https://argoproj.github.io/argo-helm"
+#   chart      = "argo-cd"
+#   version    = "8.1.1"
+#   values = [
+#     <<EOF
+#         server:
+#         service:
+#             type: LoadBalancer
+#     EOF
+#   ]
+#   depends_on = [module.eks]
+# }
