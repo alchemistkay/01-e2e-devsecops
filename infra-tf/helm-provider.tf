c2ec81fa7810a62cbb08ba35be53b7b59086e4d1
AlchemistKay <alchemistkay@k4scloud.com>
2025-06-19 12:05:14 +0000

    code fixes

--- infra-tf/helm-provider.tf
+++ infra-tf/helm-provider.tf
@@ -1,9 +1,9 @@
 data "aws_eks_cluster" "eks" {
-  name = module.eks.cluster_name
+  name = aws_eks_cluster.module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "eks" {
-  name = module.eks.cluster_name
+  name = aws_eks_cluster.module.eks.cluster_name
 }
 
 provider "helm" {
